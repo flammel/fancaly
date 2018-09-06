265c4c776cd936800c5265a496b0ea91645f24f5
Florian Lammel <mail@florianlammel.com>
2018-09-06 14:08:10 +0200

    Improve deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,8 @@ set -e
 npm run prettier
 npm run lintfix
 npm run typecheck
-rm dist/*
+rm -f dist/*
 npm run build
-cp cron.php dist/
-ssh florianlammel.com "rm /home/flammel/www/florianlammel.com/public/fancaly/*"
-scp dist/* florianlammel.com:/home/flammel/www/florianlammel.com/public/fancaly/
-ssh florianlammel.com "cd /home/flammel/www/florianlammel.com/public/fancaly/ && php cron.php"
+ssh flammel@florianlammel.com "rm -f /home/flammel/www/florianlammel.com/public/fancaly/*"
+scp dist/* flammel@florianlammel.com:/home/flammel/www/florianlammel.com/public/fancaly/
+ssh flammel@florianlammel.com "cd /home/flammel/ && php fancaly-cron.php"
