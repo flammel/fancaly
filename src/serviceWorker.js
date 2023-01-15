a47d3624ef431a2c90783c976bfac9fb9f9e5558
Florian Lammel <mail@florianlammel.com>
2023-01-15 13:10:54 +0000

    fix installability as app

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -11,3 +11,20 @@ self.addEventListener('activate', (event) => {
         caches.keys().then((keys) => Promise.all(keys.map((key) => key !== version && caches.delete(key)))),
     );
 });
+
+// A fetch listener is required so that chrome allows installing as an app
+self.addEventListener('fetch', (event) => {
+    // Special handling for index url is needed because event.request.url includes the URL fragment part,
+    // so the cache will not contain an entry for that request.
+    const url = new URL(event.request.url);
+    const request = url.pathname === '/' || url.pathname === '/index.html' ? new Request('/index.html') : event.request;
+
+    event.respondWith(
+        caches.match(request).then((cachedResponse) => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+            return fetch(request);
+        }),
+    );
+});
